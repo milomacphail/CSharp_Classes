fa0fd7031447da7e22099e36bf671a0ff36b2699
milomacphail <milomacphail@gmail.com>
2019-04-19 16:36:03 -0400

    Added Class Implementation

---
+++ Classes_Workspace/Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes_Workspace
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            Person john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+        }
+    }
+}
